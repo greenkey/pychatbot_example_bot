33cb554de65d279e64eeb74dd5f5b39c45d8a63c
Lorenzo Mele <lorenzo@loman.it>
2017-04-10 07:48:22 +0200

    all the tests

--- tests.py
+++ tests.py
@@ -2,10 +2,24 @@ import pychatbot
 import example_bot
 
 
+def test_default_echoes():
+    bot = example_bot.ExampleBot()
+
+    assert 'hello!' == bot.process('hello!')
+
+
+def test_welcome():
+    bot = example_bot.ExampleBot()
+
+    output = bot.process('/start')
+
+    assert 'welcome' in output.lower()
+
+
 def test_get_version():
     bot = example_bot.ExampleBot()
 
-    output_version = bot.version()
+    output = bot.process('/version')
 
-    assert example_bot.__version__ in output_version
-    assert pychatbot.__version__ in output_version
+    assert example_bot.__version__ in output
+    assert pychatbot.__version__ in output
