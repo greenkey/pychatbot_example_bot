3491efde32471b0dd8955ba541aed286d7423901
Lorenzo Mele <lorenzo@loman.it>
2017-04-21 17:27:29 +0200

    introducing twitter

--- run.py
+++ run.py
@@ -3,7 +3,7 @@
 import os
 import logging
 
-from pychatbot.endpoints import HttpEndpoint, TelegramEndpoint
+from pychatbot.endpoints import HttpEndpoint, TelegramEndpoint, TwitterEndpoint
 from example_bot import ExampleBot
 
 
@@ -11,7 +11,6 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 if __name__ == "__main__":
-
     bot = ExampleBot()
 
     bot.add_endpoint(HttpEndpoint(
@@ -20,6 +19,13 @@ if __name__ == "__main__":
 
     bot.add_endpoint(TelegramEndpoint(
         token=os.environ['BOT_TG_TOKEN']
+    ))
+
+    bot.add_endpoint(TwitterEndpoint(
+        consumer_key=os.environ['BOT_TW_consumer_key'],
+        consumer_secret=os.environ['BOT_TW_consumer_secret'],
+        access_token=os.environ['BOT_TW_access_token'],
+        access_token_secret=os.environ['BOT_TW_access_token_secret']
     ))
 
     bot.run()
