7559bb66f3bce7f64ef05d7747db7dc4d8dbd082
Lorenzo Mele <lorenzo@loman.it>
2017-04-22 11:42:33 +0200

    adding help and what commands

--- example_bot.py
+++ example_bot.py
@@ -7,7 +7,7 @@ import eddie
 class ExampleBot(Bot):
 
     def default_response(self, in_message):
-        return in_message
+        return self.help()
 
     @command
     def start(self):
@@ -22,3 +22,20 @@ class ExampleBot(Bot):
         my_version = "ExampleBot v%s" % __version__
         lib_version = "eddie v%s" % eddie.__version__
         return "\n".join([my_version, lib_version])
+
+    @command
+    def what(self):
+        text = """
+            eddie is a library to let you create bot in seconds.
+            You can find further information here: 
+            http://github.com/greenkey/eddie
+        """
+        return text
+
+    @command
+    def help(self):
+        text = """
+            Currently I'm not very smart, I'm just programmed with a set of commands:
+        """
+        text += '\n'.join([self.command_prepend + cmd for cmd in self.command_names])
+        return text
